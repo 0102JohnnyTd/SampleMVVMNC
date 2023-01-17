d31c22053f84e9a1fc1c1d417dbad6c75637ca25
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-17 15:47:16 +0900

    クラスファイルを作成し、Storyboard上のオブジェクトを＠IBOutletで接続

---
+++ SampleMVVMNC/Views/SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignInViewController.swift
+//  SampleMVVMNC
+//
+//  Created by Johnny Toda on 2023/01/17.
+//
+
+import UIKit
+
+final class SignInViewController: UIViewController {
+    @IBOutlet private weak var idTextField: UITextField!
+
+    @IBOutlet private weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
