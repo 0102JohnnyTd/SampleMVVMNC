7fe624663dc549c250304caba77a1f8493a8775e
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-17 15:29:13 +0900

    ViewとModelを仲介するViewModelを定義

---
+++ SampleMVVMNC/ViewModels/SignInViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  SignInViewModel.swift
+//  SampleMVVMNC
+//
+//  Created by Johnny Toda on 2023/01/17.
+//
+
+import Foundation
+import UIKit
+
+final class SignInViewMdodel {
+    // 監視元のViewのKeyとなるNotificationNameの定義
+    private let changeText = Notification.Name("changeText")
+    private let changeColor = Notification.Name("changeColor")
+
+    private var notificationCenter: NotificationCenter
+    private let signInModel: SignInModelDelegate
+
+    // イニシャライザ
+    init(notificationCenter: NotificationCenter, signInModel: SignInModelDelegate = SignInModel()) {
+        self.notificationCenter = notificationCenter
+        self.signInModel  = signInModel
+    }
+
+    // Controller側で呼び出す処理
+    func textChanged(idText: String?, passwordText: String?) {
+        let result = signInModel.validate(idText: idText, passwordText: passwordText)
+        // 結果をController側に通知
+        switch result {
+        case .success():
+            notificationCenter.post(name: changeText, object: "OK!")
+            notificationCenter.post(name: changeColor, object: UIColor.green)
+        case .failure(let error as SignInModelError):
+            notificationCenter.post(name: changeText, object: error.message)
+            notificationCenter.post(name: changeColor, object: UIColor.red)
+        case .failure(_):
+            fatalError("Unexpected Pattern")
+        }
+    }
+}
+
+// errrorに応じてControllerへ通知するメッセージを出し分ける
+extension SignInModelError {
+    var message: String {
+        switch self {
+        case .invalidId: return "IDが入力されていません"
+        case .invalidPassword: return "Passwordが入力されていません"
+        case .invalidIdAndPassword: return "IDとPasswordが入力されていません"
+        }
+    }
+}
